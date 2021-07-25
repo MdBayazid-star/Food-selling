5bc1d28d7fa09056d5677fda7863402d73942a4e
MdBayazid-star <freeelencerbayazid@gmail.com>
2021-07-25 21:30:12 +0600

    Food Selling

---
+++ js/apps.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        slidesToShow:3,
+        sliesToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+    })
+})
