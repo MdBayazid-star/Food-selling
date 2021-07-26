c7c96e30a7d0c80984ab1cfa6715db3a7442ff28
MdBayazid-star <freeelencerbayazid@gmail.com>
2021-07-26 12:30:43 +0600

    Responsive Desing

--- js/apps.js
+++ js/apps.js
@@ -5,5 +5,22 @@ $(document).ready(function(){
         sliesToScroll:1,
         prevArrow:".prev-btn",
         nextArrow:".next-btn",
+        responsive:[
+            {
+                breakpoint:992,
+                settings:{
+                    slidesToShow:2,
+                }
+            },
+            {
+                breakpoint:768,
+                settings:{
+                    slidesToShow:1,
+                }
+            }
+        ]
+    });
+    $('.nav-trigger').click(function(){
+        $('.site-content-wrapper').toggleClass('scaled')
     })
-})
+});
